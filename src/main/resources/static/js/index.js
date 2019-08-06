a66be6f4770a184b4ab1342d32a49880d4bd8105
Anya <958938239@qq.com>
2019-08-06 19:14:46 +0800

    bookmark6

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    new Swiper('.swiper-container', {
+    new Swiper('.banner-container', {
         autoplay: {
             delay: 5000
         },
@@ -10,13 +10,53 @@ $(document).ready(function () {
         speed: 600,
         parallax : true,
         scrollbar: {
-            el: '.swiper-scrollbar',
+            el: '.banner-container .swiper-scrollbar',
             hide: true,
             draggable: true
         },
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.banner-content .swiper-button-next',
+            prevEl: '.banner-content .swiper-button-prev',
+        }
+    });
+
+    var sInfo = {
+        eleWidth: 0,
+        allEleWidth: 0,
+        wrapperEleWidth: 0
+    };
+    getShortcutInfo();
+    window.addEventListener('resize',getShortcutInfo());
+    function getShortcutInfo() {
+        var $wrapper = $('.shortcut-container .swiper-wrapper');
+        var length = $wrapper.children().length;
+        var eleWidth = 0, allEleWidth = 0, width, wrapperEleWidth;
+        width = $wrapper.css('width');
+        wrapperEleWidth = parseFloat(width.slice(0, width.indexOf('px')));
+        if (length > 0) {
+            width = $('.shortcut-container .shortcut:first').css('width');
+            eleWidth = parseFloat(width.slice(0, width.indexOf('px')));
+            allEleWidth = eleWidth * length;
+        }
+        sInfo = {eleWidth: eleWidth, allEleWidth: allEleWidth, wrapperEleWidth: wrapperEleWidth};
+    }
+
+    new Swiper('.shortcut-container',{
+        freeMode: true,
+        mousewheel: true,
+        slidesPerView: 'auto',
+        freeModeMomentum: false,
+        watchSlidesProgress: true,
+        on:{
+            progress: function(progress) {
+                for (var i = 0; i <= this.slides.length; i++) {
+                    if (sInfo.allEleWidth * progress + sInfo.wrapperEleWidth * (1 - progress) >= (i + 1) * sInfo.eleWidth - 20) {
+                        $(this.slides[i]).addClass('show');
+                    } else {
+                        $(this.slides[i]).removeClass('show');
+                    }
+                }
+            }
         }
     });
 
