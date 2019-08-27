c1be4b623dcc40daa85f88481d062374a6c257b9
Anya <958938239@qq.com>
2019-08-27 19:19:08 +0800

    bookmark15

--- src/main/resources/static/js/nvi-notice.js
+++ src/main/resources/static/js/nvi-notice.js
@@ -1,4 +1,4 @@
-(function($) {
+(function($, window) {
     window.notice = {
         dom: null,
         string: ['<div class="notice"></div>', '<div class="notice-notice"><div class="notice-notice-content"><div class="notice-notice-content-wrapper"></div></div><div class="notice-notice-before"></div>'],
@@ -11,12 +11,14 @@
         open: function (params) {
             this.init();
             var e = $(this.string[1]);
-            e.find(".notice-notice-content-wrapper").html(params.content);
+            e.find('.notice-notice-content-wrapper').html(params.content);
             this.dom.append(e);
             setTimeout(function () {
                 e.children().eq(0).addClass('notice-notice-content-show');
                 e.children().eq(1).addClass('notice-notice-before-show');
                 e.find(".notice-notice-content-wrapper").addClass('notice-notice-content-wrapper-show');
+                e.find('.notice-title').addClass('notice-title-show');
+                e.find('.notice-desc').addClass('notice-desc-show');
             }, 14);
             if (typeof params.duration === 'number' && params.duration > 0) {
                 var that = this;
@@ -27,12 +29,15 @@
             return e;
         },
         close: function (e) {
+            e.addClass('notice-notice-hidden');
             e.children().eq(0).removeClass('notice-notice-content-show');
             e.children().eq(1).removeClass('notice-notice-before-show');
-            e.find(".notice-notice-content-wrapper").removeClass('notice-notice-content-wrapper-show');
+            e.find('.notice-notice-content-wrapper').removeClass('notice-notice-content-wrapper-show');
+            e.find('.notice-title').removeClass('notice-title-show');
+            e.find('.notice-desc').removeClass('notice-desc-show');
             setTimeout(function () {
                 e.remove();
             }, 600);
         }
     };
-})(jQuery);
+})(jQuery, window);
