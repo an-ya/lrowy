3182b551f5403c6193da014fd0aa7763679ac767
Anya <958938239@qq.com>
2019-09-25 19:09:59 +0800

    index9

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,7 +3,7 @@ var page = 2, pageMin = 1, mode = 2, pageMax = Math.ceil($('.article-category-it
 var $parallax = $('.parallax'), parallax = [], $viewP = $('.view-p'), viewP = [];
 $parallax.css('visibility', 'visible');
 for (var i = 0; i < $parallax.length; i++) { parallax.push(parallaxInitOne($($parallax[i]))); }
-for (i = 0; i < $viewP.length; i++) { viewP.push($($viewP[i])); }
+for (i = 0; i < $viewP.length; i++) { viewP.push(viewInitOne($($viewP[i]))); }
 scrollEvent();
 
 $(document).scroll(scrollEvent);
@@ -152,42 +152,57 @@ function getViewResult($this) {
     }
 }
 
-function viewOne ($this) {
+function viewInitOne ($this) {
+    var children = [];
     var target = $this.data('viewTarget');
     var attr = $this.data('viewAttr');
     var mode = $this.data('viewMode');
+    var delay = $this.data('viewDelay');
     var top = $this.offset().top, bottom = $this.height() + top;
 
     if (target === 'self') target = $this;
     else target = $this.children();
     if (mode === 'static') top = 10000;
 
-    if (scrollTop > top || scrollTop + vh < bottom) {
-        target.each(function () {
-            var _this = $(this);
-            var css;
-            if (attr === 'children') css = getViewResult(_this);
-            else css = getViewResult($this);
-            if (css.type === 1) {
-                _this.removeClass(css.animIn);
-                _this.addClass(css.animOut);
+    target.each(function () {
+        var _this = $(this);
+        var css;
+        if (attr === 'children') css = getViewResult(_this);
+        else css = getViewResult($this);
+        _this['_css'] = css;
+        children.push(_this);
+    });
+
+    $this['_target'] = children;
+    $this['_attr'] = attr;
+    $this['_mode'] = mode;
+    $this['_delay'] = delay;
+    $this['_top'] = top;
+    $this['_bottom'] = bottom;
+    return $this;
+}
+
+function viewOne ($this) {
+    var target = $this['_target'];
+    var top = $this['_top'], bottom = $this['_bottom'];
+
+    for (var i = 0; i < target.length; i++) {
+        var css = target[i]['_css'];
+        if (css.type === 1) {
+            if (scrollTop > top || scrollTop + vh < bottom) {
+                target[i].removeClass(css.animIn);
+                target[i].addClass(css.animOut);
             } else {
-                _this.css(css.cssE);
+                target[i].removeClass(css.animOut);
+                target[i].addClass(css.animIn);
             }
-        });
-    } else {
-        target.each(function () {
-            var _this = $(this);
-            var css;
-            if (attr === 'children') css = getViewResult(_this);
-            else css = getViewResult($this);
-            if (css.type === 1) {
-                _this.removeClass(css.animOut);
-                _this.addClass(css.animIn);
+        } else {
+            if (scrollTop > top || scrollTop + vh < bottom) {
+                target[i].css(css.cssE);
             } else {
-                _this.css(css.cssO);
+                target[i].css(css.cssO);
             }
-        });
+        }
     }
 }
 
@@ -198,10 +213,8 @@ function scrollToMain (index) {
 
     for (var i = 0; i < viewP.length; i++) {
         var $this = viewP[i];
-        // var mode = $this.data('viewMode');
-        var mode;
-        if (i === 1) mode = 'static';
-        var top = $this.offset().top, bottom = $this.height() + top;
+        var mode = $this['_mode'];
+        var top = $this['_top'], bottom = $this['_bottom'];
         if ((scrollTop < top && scrollTop + vh > bottom && target > top && mode !== 'static') || (scrollTop < top && scrollTop + vh > bottom && target + vh < bottom )) {
             begin.push($this);
         } else if ((scrollTop > top && target + vh > bottom && target < top && mode !== 'static') || (scrollTop + vh < bottom && target + vh > bottom && target < top)) {
@@ -212,11 +225,11 @@ function scrollToMain (index) {
     scrollTop = target;
     for (i = 0; i < begin.length; i++) {
         setTimeout(function (i) { viewOne(begin[i]); }, t, i);
-        delay = begin[i].data('viewDelay');
+        delay = begin[i]['_delay'];
         if (delay !== undefined) t += delay;
     }
     setTimeout(function () {
-        $('html,body').stop().animate({scrollTop: vh * index}, 1200, 'swing', function () {
+        $('html,body').stop().animate({scrollTop: vh * index}, 1000, 'swing', function () {
             for (var i = 0; i < end.length; i++) { viewOne(end[i]); }
             isScrolling = false;
         });
@@ -317,7 +330,7 @@ function parallaxOne($this) {
             if (r === 1) translateX = (offset - scrollTop) * x / range + 'px';
         }
         if (r === 2) translateX = (scrollTop - offset) * x / range + 'px';
-        if (mode !== undefined && r === 3) translateX = x + 'px';
+        if (r === 3) translateX = x + 'px';
     }
     if (y !== undefined) {
         if (mode === 'symmetry') {
@@ -333,11 +346,11 @@ function parallaxOne($this) {
     }
 
     if (translateX && translateY) {
-        translate =  'translate(' + translateX + ',' + translateY + ')';
+        translate =  'translate3d(' + translateX + ',' + translateY + ',0)';
     } else if (translateX) {
-        translate = 'translate(' + translateX + ',0)';
+        translate = 'translate3d(' + translateX + ',0,0)';
     } else if (translateY) {
-        translate = 'translate(0,' + translateY + ')';
+        translate = 'translate3d(0,' + translateY + ',0)';
     }
 
     if (scaleO !== undefined && scaleE !== undefined) {
@@ -347,7 +360,7 @@ function parallaxOne($this) {
             if (r === 1) scale = (offset - scrollTop) / range * (scaleE - scaleO) + scaleO;
         }
         if (r === 2) scale = (scrollTop - offset) / range * (scaleE - scaleO) + scaleO;
-        if (mode !== undefined && r === 3) scale = scaleE;
+        if (r === 3) scale = scaleE;
         scale = 'scale(' + scale + ')';
     }
 
@@ -367,7 +380,7 @@ function parallaxOne($this) {
             opacity = opacityO;
         }
         if (r === 2) opacity = (scrollTop - offset) / range * (opacityE - opacityO) + opacityO;
-        if (mode !== undefined && r === 3) opacity = opacityE;
+        if (r === 3) opacity = opacityE;
         if (opacity > 1) opacity = 1;
         css['opacity'] = opacity;
     }
